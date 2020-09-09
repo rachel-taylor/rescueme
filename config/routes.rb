d689dcb7d2d615e52374ee7ebc63db1430fa0113
rachel-taylor <taylor.d.rachel@gmail.com>
2020-09-08 21:39:12 -0700

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
+  get 'about', to: 'posts#about'
 end
---
+++ db/migrate/20200909043755_create_note.rb
@@ -0,0 +1,8 @@
+class CreateNote < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notes do |t|
+      t.string title
+      t.text description 
+    end
+  end
+end
