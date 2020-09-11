1f8a791cb5ecd92b70940e4cd131624b6e90e911
rachel-taylor <taylor.d.rachel@gmail.com>
2020-09-10 22:14:07 -0700

    Updated Note View

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,15 @@ class NotesController < ApplicationController
     def new 
         @note = Note.new
     end 
+
+    def create
+        @note = Note.new(note_params)
+        @note.save
+        redirect_to note_path(@note)
+    end 
+
+    private 
+    def note_params
+        params.require(:note).permit(:title, :description)
+    end 
 end 
