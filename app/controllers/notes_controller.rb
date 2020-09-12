93312303971956f5df65b5be4dd492f974f5b321
rachel-taylor <taylor.d.rachel@gmail.com>
2020-09-12 12:17:56 -0700

    Updated Controllers

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,9 @@
 class NotesController < ApplicationController
-    
+    def index
+        @note = Note.all
+        #modview of all comments made
+    end 
+
     def new 
         @note = Note.new
     end 
@@ -7,12 +11,28 @@ class NotesController < ApplicationController
     def create
         @note = Note.new(note_params)
         @note.save
-        redirect_to '/notes/show'
-        byebug 
+        redirect_to '/notes'
+        
     end 
 
-    def show   
+    def show  
+        #byebug
+        redirect_to '/notes/index'
     end 
+
+    def edit
+        @note = Note.find([params:id])
+    end 
+
+    def update
+        @note = Note.find([params:id])
+        if @note.update(note.params)
+            redirect_to '/notes/show'
+        else 
+            redirect_to 'notes/edit/'
+        end 
+    end 
+
 
     private 
     def note_params
