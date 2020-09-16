5050c6ea9fb81bc963767458ef86f9ca4122b39a
rachel-taylor <taylor.d.rachel@gmail.com>
2020-09-15 21:57:24 -0700

    Updated Models

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,5 @@ class Note < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true
     validates :description, presence: true, length: {minimum: 20, maximum: 500}
-    validates :user_id, presence: true 
+    #validates :user_id, presence: true 
 end 
